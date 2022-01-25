3771986df9e60f3460dd664754bb6af34126c4c4
aaatipamula <aaatipamula@gmail.com>
2022-01-25 00:09:31 -0600

    using ytdl module now

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,37 +1,38 @@
 import os
-from pytube import YouTube
-from moviepy.editor import *
-from tqdm import tqdm
+import sys
 import json
+import youtube_dl
+
+if __name__ == "__main__":
+    x = input("YOU ARE STARTING THIS PROGRAM ARE YOU SURE YOU WANT TO START [y/n]: ")
+    if x == "yes" or x == "y":
+        pass
+    else:
+        print("STOPPING PROGRAM...")
+        exit()
+
 
 data = json.load(open('path.json', 'r'))
 
 os.chdir(data.get("music_dir"))
-print(f"Changing directory to {os.getcwd()}...")
+print(f"\nChanging directory to {os.getcwd()}...")
 
-with open("videos.txt", "r") as f:
-    videos = f.readlines()
+ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': '%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+        }
+try:
+    i = input("Please input the url of the playlist/video you would like to download\n>> ")
 
-for x in videos:
-    x.replace("\n", ".")
-
-    yt = YouTube(str(x))
-
-    print("Downloading video...")
-    for i in tqdm(range(100)):
-        video = yt.streams.filter(file_extension = 'mp4').get_highest_resolution().download(output_path= data.get("video_dir"))
-    print(f"{yt.title}.mp4 downloaded.")
-
-    print("Downloading audio...")
-    for i in tqdm(range(100)):
-        audiofile = yt.streams.filter(only_audio= True, abr= "128kbps").first().download()
-    print(f"{yt.title} audio downloaded.")
-
-    outfile = audiofile.replace("mp4", "mp3")
-
-    with AudioFileClip(audiofile) as a:
-        a.write_audiofile(outfile)
-
-    os.remove(audiofile)
-
-    print(f"{yt.title} is downloaded and converted!\n")
+    if i.startswith("https://www.youtube.com/") == False:
+        raise Exception("Not a youtube url.")
+    else:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([i])
+except Exception:
+    sys.exc_clear()
